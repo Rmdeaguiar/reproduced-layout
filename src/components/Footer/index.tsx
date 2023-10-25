82d755ed15bacc3046cebf476fecae3713a2d217
Rafael <rafael.aguiar91@gmail.com>
2023-10-25 17:24:21 -0300

    feat: conclusão do footer e ajustes de css

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -21,6 +21,31 @@ function Footer() {
           <div className="circle-media"><Twitter /></div>
         </div>
       </div>
+      <div className="second-footer">
+        <div className="contacts">
+          <div>
+            <h3>For general inquiries on our services</h3>
+            <h2>hello@summerlymedia.com</h2>
+          </div>
+          <div>
+            <h3>To apply as a content creator or team member</h3>
+            <h2>creatores@summerlymedia.com</h2>
+          </div>
+        </div>
+        <div className="links-footer">
+        <nav className='services-footer'>
+            <a>Portfolio</a>
+            <a>What we offer</a>
+            <a>How it works</a>
+            <a>Contact us</a>
+          </nav>
+          <nav className='rights-footer'>
+            <a>Privacy</a>
+            <a>Terms</a>
+          </nav>
+        </div>
+      
+      </div>
 
     </footer>
 
