6d0d604a79bc2f309f4aeb7fb83ce6b163117f09
Rafael <rafael.aguiar91@gmail.com>
2023-10-25 14:45:30 -0300

    feat: section about client

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
 import './App.scss';
 import { Logo } from './components/logo';
 import { Arrow } from './components/arrow-icon';
+import { useState } from 'react';
+import { Interface } from 'readline';
 
 function App() {
+
+  type About = {
+    title: string,
+    emoji: string
+  }
+
+  const aboutClient: About[] = [
+    { title: 'Products', emoji: '📦' }, { title: 'Services', emoji: '🤝' }, { title: 'SaaS', emoji: '🖥️' }, { title: 'Personality', emoji: '🤳' }, { title: 'Other', emoji: '🤔' }
+  ]
+
+
+
+
   return (
     <main>
       <header className='header-container'>
@@ -33,6 +48,26 @@ function App() {
           <div className="bubble shopping">🛍️</div>
           <div className="bubble rocket">🚀</div>
           <div className="bubble fire">🔥</div>
+        </div>
+      </div>
+      <div className="customer-container">
+        <div className="heading-customer">
+          <h1>We want to get to know you 😊</h1>
+          <h3>01/05</h3>
+        </div>
+        <div className="info-customer">
+          <h3>Hi!😉 What best explains you?</h3>
+        </div>
+        <div className="options-container">
+          {aboutClient.map((about) => (
+            <div key={about.title} className='about-customer'>
+              <p>{about.title}</p>
+              <p>{about.emoji}</p>
+            </div>
+          ))}
+        </div>
+        <div className='footer-customer'>
+        <button>Next</button>
         </div>
       </div>
     </main>
