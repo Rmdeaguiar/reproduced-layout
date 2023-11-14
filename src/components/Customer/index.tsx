788602154c9c5e52d2147497ece4fe17475f2d6a
Rafael <rafael.aguiar91@gmail.com>
2023-11-14 11:26:41 -0300

    feat: adição das páginas 3 e 4 sobre o cliente

--- src/components/Customer/index.tsx
+++ src/components/Customer/index.tsx
@@ -16,23 +16,30 @@ function Customer() {
   //const startClient: string[] = ['Yesterday', 'Within 2 weeks', 'Within a few months', 'Not for a while'];
 
 
-function handleBtnNext () {
-  setPage(2)
-}
+  function handleBtnNext() {
+    if (page < 4) {
+      setPage(page + 1)
+    }
+  }
 
 
-function handleBtnPrev () {
-  setPage(1)
-}
+  function handleBtnPrev() {
+    if (page !== 1) {
+      setPage(page - 1)
+    }
+  }
 
   return (
     <div className="customer-container">
       <div className="heading-customer">
         <h1>We want to get to know you 😊</h1>
-        <h3>{page}/2</h3>
+        <h3>{page}/4</h3>
       </div>
       <div className="info-customer">
-      <h3>{page === 1 ? 'Hi!😉 What best explains you?' : 'Nice👍 What is your monthly budget?'}</h3>
+        {page === 1 && <h3>Hi!😉 What best explains you?</h3>}
+        {page === 2 && <h3>Nice👍 What is your monthly budget?</h3>}
+        {page === 3 && <h3>Anything else you want us to know?</h3>}
+        {page === 4 && <h3>Great!🙌 What's your name?</h3>}
       </div>
       <div className="options-container">
         {page === 1 ? aboutClient.map((about) => (
@@ -40,30 +47,39 @@ function handleBtnPrev () {
             <p>{about.title}</p>
             <p>{about.emoji}</p>
           </div>
-        )) : 
-        aboutClient.map((about) => (
-          <div key={about.title} className={'about-customer disappear-down'}>
-            <p>{about.title}</p>
-            <p>{about.emoji}</p>
-          </div>
-        ))}
-
-        {page === 2 ? 
-           budgetClient.map((budget)=>(
-            <div key={budget} className={'about-customer appear-up'}> 
-            <p>{budget}</p>
+        )) :
+          aboutClient.map((about) => (
+            <div key={about.title} className={'about-customer disappear-down'}>
+              <p>{about.title}</p>
+              <p>{about.emoji}</p>
             </div>
-          )) : 
-          budgetClient.map((budget)=>(
-            <div key={budget} className={'about-customer disappear-down'}> 
-            <p>{budget}</p>
+          ))}
+        {page === 2 ?
+          budgetClient.map((budget) => (
+            <div key={budget} className={'about-customer appear-up'}>
+              <p>{budget}</p>
+            </div>
+          )) :
+          budgetClient.map((budget) => (
+            <div key={budget} className={'about-customer disappear-down'}>
+              <p>{budget}</p>
             </div>
           ))
-          }
+        }
+        {page === 3 &&
+          <textarea placeholder='This is optional...'></textarea>
+        }
+        {page === 4 &&
+          <div className='contacts-client'>
+            <input placeholder='Name'></input>
+            <input placeholder='Company'></input>
+            <input placeholder='Email'></input>
+          </div>
+        }
       </div>
       <div className='footer-customer'>
-        <button className={`btn-previous ${page === 1 ? "" : "btn-opacity" }`} onClick={() => handleBtnPrev()}>Previous</button>
-        <button className='btn-next' onClick={() => handleBtnNext()}>Next</button>
+        <button className={`btn-previous ${page === 1 ? "" : "btn-opacity"}`} onClick={() => handleBtnPrev()}>Previous</button>
+        <button onClick={() => handleBtnNext()}>{page === 4 ? 'Submit' : 'Next'}</button>
       </div>
     </div>
   );
